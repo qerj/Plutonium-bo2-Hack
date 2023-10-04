57d409b7f8c13e1d1db719049f0cf9aed1a71b6b
Judah <118308925+qerj@users.noreply.github.com>
2023-10-03 21:50:17 -0400

    Yes

---
+++ Plutonium-Hecking/Plutonium-Hecking/Plutonium-Hecking.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <Windows.h>
+
+
+float normalFov = 106.0f;
+
+DWORD WINAPI Trainer(LPVOID param)
+{
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
+
+	uintptr_t plutoniumModule = (uintptr_t)GetModuleHandleA("plutonium-bootstrapper-win32.exe");
+
+	float fov = 106.0f;
+	bool noHud = false;
+
+	DWORD old_protect;
+	unsigned char* noHudAddress = (unsigned char*)(plutoniumModule + 0x1DE53A);
+	VirtualProtect((void*)noHudAddress, 3, PAGE_EXECUTE_READWRITE, &old_protect);
+
+	while (!GetAsyncKeyState(VK_END))
+	{
+		
+		*(float*)(plutoniumModule + 0x2641440) = fov;
+
+
+		if (GetAsyncKeyState(VK_F1) & 1)
+		{
+			fov++;
+		}
+		if (GetAsyncKeyState(VK_F2) & 1)
+		{
+			fov--;
+		}
+
+		if (GetAsyncKeyState(VK_F3) & 1)
+		{
+			noHud = !noHud;
+		}
+
+		if (noHud) {
+			*noHudAddress = 0xC1;
+			*(noHudAddress + 1) = 0xE0;
+			*(noHudAddress + 2) = 0x4;
+			//0xC1 0xE0 0x04
+		}
+		else if (!noHud) {
+			*(noHudAddress + 2) = 0x15;
+			//0xC1 0xE0 0x15
+		}
+	}
+
+	//reset our hacks
+	*(float*)(plutoniumModule + 0x2641440) = normalFov;
+	*noHudAddress = 0xC1;
+	*(noHudAddress + 1) = 0xE0;
+	*(noHudAddress + 2) = 0x4;
+
+	fclose(f);
+	FreeConsole();
+	FreeLibraryAndExitThread((HMODULE)param, 0); 
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_THREAD_ATTACH:  break;
+	case DLL_THREAD_DETACH:  break;
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Trainer, hModule, 0, nullptr);
+		DisableThreadLibraryCalls(hModule);
+		break;
+	}
+	return TRUE;
+}
